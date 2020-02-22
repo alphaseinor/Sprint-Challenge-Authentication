faf669889d858f42bbd2392414a80d3e72494a70
Brian Hague <alphaseinor@gmail.com>
2020-02-21 21:14:29 -0600

    added validate middleware that encrypts req.body.password

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -4,14 +4,22 @@ const jwt = require('jsonwebtoken')
 
 const Users = require('./users-model')
 
-
-
 router.post('/register', (req, res) => {
-  res.status(200).json(req.body)
+  //res.status(200).json(req.body)
+  req.body.password = bcrypt.hashSync(req.body.password, 8)
+
 })
 
 router.post('/login', (req, res) => {
   res.status(200).json(req.body)
 })
+
+function validateMe(req, res, next) {
+  if(req.body.password && req.body.username){
+    next()
+  }else{
+    res.status(500).json({message: "please enter username and password"})
+  }
+}
 
 module.exports = router;
